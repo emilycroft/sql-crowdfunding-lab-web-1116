32943c57f38c631efac9bfc0c4f7d9cda6f6cb7e
emilycroft <emilyhcroft@gmail.com>
2016-12-08 14:15:01 -0500

    done

---
+++ lib/insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO projects (title) VALUES ("Blah1"), ("Blah2"), ("Blah3"), ("Blah4"), ("Blah5"), ("Blah6"), ("Blah7"), ("Blah8"), ("Blah9"), ("Blah10");
+
+
+INSERT INTO users (name) VALUES ("name"), ("name"), ("name"), ("name"), ("name"), ("name"), ("name"), ("name"), ("name"), ("name"), ("name"), ("name"), ("name"), ("name"), ("name"), ("name"), ("name"), ("name"), ("name"), ("name");
+
+
+INSERT INTO pledges (amount) VALUES (4), (4), (4), (4), (4), (4), (4), (4), (4), (4), (4), (4), (4), (4), (4), (4), (4), (4), (4), (4), (4), (4), (4), (4), (4), (4), (4), (4), (4), (4);
